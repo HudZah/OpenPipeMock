33af615cc32d51f477dc58adcac41dd44b9c2b20
Soham Basu <sohambasu963@gmail.com>
2023-10-17 18:13:00 -0400

    feat: added LLM navbar and prettier changes

---
+++ client/src/app/dashboard/page.tsx
@@ -0,0 +1,11 @@
+import React from 'react';
+import { Box, Typography } from '@mui/material';
+import Bar from './components/LLMBar';
+
+export default function Home() {
+  return (
+    <Box>
+      <Bar />
+    </Box>
+  );
+}
--- client/src/app/page.tsx
+++ client/src/app/page.tsx
@@ -1,6 +1,6 @@
-import { CardLogin } from "@/app/login/components/card_login";
-import { Button } from "@/components/ui/button";
+import { CardLogin } from '@/app/login/components/card_login';
+import { Button } from '@/components/ui/button';
 
 export default function Home() {
-    return <h1>Main Page</h1>;
+  return <h1>Main Page</h1>;
 }
