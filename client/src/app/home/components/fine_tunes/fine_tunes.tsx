04ba6f86c35ea63d182ab2ac626be5686eb7e635
Hudzah <chiefhudhayfa@gmail.com>
2023-12-24 11:39:48 -0500

    awfawf

--- client/src/app/home/components/fine_tunes/fine_tunes.tsx
+++ client/src/app/home/components/fine_tunes/fine_tunes.tsx
@@ -206,7 +206,7 @@ export default function FineTunes() {
       <div className="hidden h-full flex-1 flex-col space-y-8 p-8 md:flex">
         <div className="flex items-center justify-between space-y-2">
           <div>
-            <h2 className="text-2xl font-bold tracking-tight">Welcome back!</h2>
+            <h2 className="text-2xl font-bold tracking-tight">Welcome back</h2>
             <p className="text-muted-foreground">
               Here&apos;s a list of your logs!
             </p>
