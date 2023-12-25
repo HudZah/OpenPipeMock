4f0eec1c3e5027fcb913b00aee53d94c9e9d26ea
Hudzah <chiefhudhayfa@gmail.com>
2023-12-24 20:52:33 -0500

    Update fine_tunes.tsx component

--- client/src/app/home/components/fine_tunes/fine_tunes.tsx
+++ client/src/app/home/components/fine_tunes/fine_tunes.tsx
@@ -206,9 +206,11 @@ export default function FineTunes() {
       <div className="hidden h-full flex-1 flex-col space-y-8 p-8 md:flex">
         <div className="flex items-center justify-between space-y-2">
           <div>
-            <h2 className="text-2xl font-bold tracking-tight">Welcome back</h2>
+            <h2 className="text-2xl font-bold tracking-tight">
+              Your Finetunes
+            </h2>
             <p className="text-muted-foreground">
-              Here&apos;s a list of your logs!
+              Here&apos;s a list of your finetunes!
             </p>
           </div>
           <div className="flex items-center space-x-2">
